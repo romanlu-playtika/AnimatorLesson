dcb2b5cb1907da712d908c639c1393bcb375dab5
romanlu <romanlu@playtika.com>
2020-10-27 20:41:49 +0300

    Added button on "Jim" scene

---
+++ Animation/Assets/GameCode/Jim/JimButtonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Animator))]
+public class JimButtonController : MonoBehaviour
+{
+    private Animator anim;
+    private int flyFloat = Animator.StringToHash("Fly");
+    private int scaleFloat = Animator.StringToHash("Scale");
+    private int shakeFloat = Animator.StringToHash("Shake");
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void PickRandomAnimation()
+    {
+        var rand = Random.Range(0.0f, 1.0f);
+        anim.SetFloat(flyFloat, rand);
+        rand = Random.Range(0.0f, 1.0f);
+        anim.SetFloat(scaleFloat, rand);
+        rand = Random.Range(0.0f, 1.0f);
+        anim.SetFloat(shakeFloat, rand);
+    }
+}
