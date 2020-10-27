e598f9ceb78d5ef88077b9b237af3c2d94bbf605
romanlu <romanlu@playtika.com>
2020-10-27 12:40:35 +0300

    Animations initial

--- Animation/Assets/GameCode/JimController.cs
+++ Animation/Assets/GameCode/JimController.cs
@@ -6,6 +6,9 @@ public class JimController : MonoBehaviour
     [SerializeField] private float walkSpeed = 1f;
     private Animator anim;
     private SpriteRenderer spriteRenderer;
+    private int isJumping = Animator.StringToHash("isJumping");
+    private int isMoving = Animator.StringToHash("isMoving");
+    private int isCrouching = Animator.StringToHash("isCrouching");
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
@@ -15,23 +18,38 @@ public class JimController : MonoBehaviour
     void Update()
     {
         Move();
-        if ((Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.UpArrow)) && !anim.GetBool("isJumping"))
+        if ((Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            && !anim.GetBool(isJumping))
         {
             Jump();
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.C))
+        {
+            Crouch(true);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.C))
+        {
+            Crouch(false);
         }
     }
 
     public void Jump()
     {
-        anim.SetBool("isJumping", !anim.GetBool("isJumping"));
+        anim.SetBool(isJumping, !anim.GetBool(isJumping));
     }
 
     private void Move()
     {
         var input = Input.GetAxis("Horizontal");
         var direction = new Vector2(input, transform.position.y);
-        anim.SetBool("isMoving", input != 0.0f);
+        anim.SetBool(isMoving, input != 0.0f);
         spriteRenderer.flipX = (input < 0);
         transform.Translate(direction * walkSpeed * Time.deltaTime);
+    }
+
+    private void Crouch(bool crouchBool)
+    {
+        anim.SetBool(isCrouching, crouchBool);
     }
 }
