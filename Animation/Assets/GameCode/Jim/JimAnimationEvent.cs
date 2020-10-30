525557ffe0f463620a37b991e5490322b86a2d8b
romanlu <romanlu@playtika.com>
2020-10-30 09:28:25 +0300

    Physics initial

--- Animation/Assets/GameCode/Jim/JimAnimationEvent.cs
+++ Animation/Assets/GameCode/Jim/JimAnimationEvent.cs
@@ -8,19 +8,19 @@ public class JimAnimationEvent : MonoBehaviour
 {
     private JimController jimController;
     private AudioSource audioSource;
+    private Animator animator;
+    private int isJumping = Animator.StringToHash("isJumping");
 
     private void Awake()
     {
         jimController = GetComponentInParent<JimController>();
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     private void JumpStop()
     {
-        if (jimController != null)
-        {
-            jimController.Jump();
-        }
+        animator.SetBool(isJumping, false);
     }
 
     private void PlaySound(AudioClip clip)
--- Animation/Assets/GameCode/Jim/JimController.cs
+++ Animation/Assets/GameCode/Jim/JimController.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class JimController : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 1.0f;
-
+    [SerializeField] private float jumpSpeed = 1.0f;
+    [SerializeField] LayerMask layerMaskForGrounded;
+    [SerializeField] float isGroundedRayLength =0.1f;
+    
     private Animator anim;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigbody2D;
+
     private int isJumping = Animator.StringToHash("isJumping");
     private int isMoving = Animator.StringToHash("isMoving");
     private int isCrouching = Animator.StringToHash("isCrouching");
     private int isIdle = Animator.StringToHash("isIdle");
+    
+    private bool isGrounded {
+        get {
+            Vector3 position = transform.position;
+            position.y = GetComponent<Collider2D> ().bounds.min.y + 0.1f;
+            float length = isGroundedRayLength + 0.1f;
+            Debug.DrawRay (position, Vector3.down * length);
+            return Physics2D.Raycast (position, Vector3.down, length, layerMaskForGrounded.value);
+        }
+    }
+
+    private float currentJumpSpeed;
 
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rigbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
@@ -28,15 +46,13 @@ public class JimController : MonoBehaviour
         EventManager.OnIdleStayEvent -= Idle;
     }
 
-    void Update()
+    private void Update()
     {
-        Move();
         if ((Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
             && !anim.GetBool(isJumping))
         {
             Jump();
         }
-
         if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.C))
         {
             Crouch(true);
@@ -47,19 +63,23 @@ public class JimController : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+        Move();
+    }
+
     public void Jump()
     {
-        anim.SetBool(isJumping, !anim.GetBool(isJumping));
+        rigbody2D.velocity = new Vector2(rigbody2D.velocity.x, jumpSpeed);
+        anim.SetBool(isJumping, true);
     }
 
     private void Move()
     {
         var input = Input.GetAxis("Horizontal");
-        var direction = new Vector2(input, transform.position.y);
-
         anim.SetBool(isMoving, input != 0.0f);
         spriteRenderer.flipX = (input < 0);
-        transform.Translate(direction * walkSpeed * Time.deltaTime);
+        rigbody2D.velocity = new Vector2(input * walkSpeed, rigbody2D.velocity.y);
     }
 
     private void Crouch(bool crouchBool)
